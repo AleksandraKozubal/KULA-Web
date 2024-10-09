c44a68e9949e4be24eb8209eaf21bee282942782
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-10-09 23:12:22 +0200

    wip initialization

---
+++ resources/views/filament/logo.blade.php
@@ -0,0 +1,9 @@
+<div class="flex items-center gap-3">
+    <div>
+        <img src="{{ asset("identification/sygnet.svg") }}" alt="KULA admin panel" class="w-10">
+    </div>
+    <div>
+        <span class="font-medium">KULA</span>
+        <span>website panel</span>
+    </div>
+</div>
