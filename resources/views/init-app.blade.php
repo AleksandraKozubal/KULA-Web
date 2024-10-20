50809079ac96099b4bad827664ca022eecd36d57
zmigrOO <100092805+zmigrOO@users.noreply.github.com>
2024-10-20 21:20:41 +0200

    Rename username field to name

--- resources/views/init-app.blade.php
+++ resources/views/init-app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Init App</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/filament/filament/app.css') }}">
     @vite('resources/css/app.css')
 </head>
 
@@ -26,9 +26,10 @@
                         </h2>
                         <form method="POST" action=""
                             class="dark:[&>div>input]:bg-gray-900 dark:[&>div>input]:text-white [&>div>input]:h-10 *:*:transition">
+                            @csrf
                             <div class="mb-4">
-                                <label for="username" class="block text-gray-700 dark:text-gray-300">Name</label>
-                                <input id="username" type="text" name="username"
+                                <label for="name" class="block text-gray-700 dark:text-gray-300">Name</label>
+                                <input id="name" type="text" name="name"
                                     class="block w-full mt-1 border-gray-300 rounded-md shadow-sm dark:border-gray-700"
                                     required>
 
@@ -56,7 +57,7 @@
                             </div>
                             <div class="text-center">
                                 <button disabled type="submit"
-                                    class="px-4 py-2 text-white transition rounded-md bg-kula-light-500 hover:bg-kula-light-600 disabled:bg-kula-light-300">Create
+                                    class="px-4 py-2 text-white transition rounded-md dark:text-black bg-kula-light-500 dark:bg-kula-dark-600 hover:bg-kula-light-600 dark:hover:bg-kula-dark-700 disabled:bg-kula-light-300 dark:disabled:bg-kula-dark-100">Create
                                     Admin Account</button>
                             </div>
                         </form>
@@ -73,7 +74,7 @@
     const password_confirmation = document.getElementById('password_confirmation');
     const submitButton = document.querySelector('button[type="submit"]');
     const email = document.getElementById('email');
-    const username = document.getElementById('username');
+    const name = document.getElementById('name');
     const passwordRegex =
         /^((?=.*[0-9])(?=.*[a-z])(?=.*([A-Z]|[!-\/:-@[-`{-~]))|(?=.*[A-Z])(?=.*[!-\/:-@[-`{-~])(?=.*([0-9]|[a-z])))[a-zA-Z0-9!-\/:-@[-`{-~]{8,}$/;
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
@@ -83,6 +84,47 @@
             submitButton.removeAttribute('disabled');
         } else {
             submitButton.setAttribute('disabled', 'disabled');
+        }
+    }
+
+    function checkUsername(skipColors = 0) {
+        if (name.value.length > 0) {
+            valid.name = true;
+            if(!skipColors) name.classList.remove('!border-red-500');
+        } else {
+            valid.name = false;
+            if(!skipColors)name.classList.add('!border-red-500');
+        }
+    }
+
+    function checkEmail(skipColors = 0) {
+        if (emailRegex.test(email.value)) {
+            valid.email = true;
+            if(!skipColors) email.classList.remove('!border-red-500');
+        } else {
+            valid.email = false;
+            if(!skipColors) email.classList.add('!border-red-500');
+        }
+    }
+
+    function checkPassword(skipColors = 0) {
+        if (passwordRegex.test(password.value)) {
+            valid.password = true;
+            if(!skipColors) password.classList.remove('!border-red-500');
+        } else {
+            valid.password = false;
+            if(!skipColors) password.classList.add('!border-red-500');
+        }
+        checkPasswordConfirmation(skipColors);
+    }
+
+    function checkPasswordConfirmation(skipColors = 0) {
+        if (password.value === password_confirmation.value) {
+            valid.password_confirmation = true;
+            if(!skipColors)password_confirmation.classList.remove('!border-red-500');
+        } else {
+            valid.password_confirmation = false;
+            if(!skipColors)password_confirmation.classList.add('!border-red-500');
         }
     }
     let valid = [{
@@ -99,53 +141,26 @@
         }
     ];
 
-    username.addEventListener('input', () => {
-        if (username.value.length > 0) {
-            valid.name = true;
-            username.classList.remove('!border-red-500');
-        } else {
-            valid.name = false;
-            username.classList.add('!border-red-500');
-        }
+    name.addEventListener('input', () => {
+        checkUsername();
         checkValidation();
     });
     email.addEventListener('input', () => {
-        if (emailRegex.test(email.value)) {
-            valid.email = true;
-            email.classList.remove('!border-red-500');
-        } else {
-            valid.email = false;
-            email.classList.add('!border-red-500');
-        }
+        checkEmail();
         checkValidation();
     });
     password.addEventListener('input', () => {
-        if (passwordRegex.test(password.value)) {
-            valid.password = true;
-            password.classList.remove('!border-red-500');
-        } else {
-            valid.password = false;
-            password.classList.add('!border-red-500');
-        }
-        if (password.value === password_confirmation.value) {
-            valid.password_confirmation = true;
-            password_confirmation.classList.remove('!border-red-500');
-        } else {
-            valid.password_confirmation = false;
-            password_confirmation.classList.add('!border-red-500');
-        }
+        checkPassword();
         checkValidation();
     });
     password_confirmation.addEventListener('input', () => {
-        if (password.value === password_confirmation.value) {
-            valid.password_confirmation = true;
-            password_confirmation.classList.remove('!border-red-500');
-        } else {
-            valid.password_confirmation = false;
-            password_confirmation.classList.add('!border-red-500');
-        }
+        checkPasswordConfirmation();
         checkValidation();
     });
+    checkUsername(1);
+    checkEmail(1);
+    checkPassword(1);
+    checkValidation();
 </script>
 
 </html>
