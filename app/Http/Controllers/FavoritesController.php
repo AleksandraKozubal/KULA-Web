6d1142890ac75f48a0c86d0a7714761cd08dbf8f
zmigrOO <100092805+zmigrOO@users.noreply.github.com>
2024-10-22 21:10:47 +0200

    Create API endpoints Closes #2

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Favorites;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Favorites $favorites)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Favorites $favorites)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Favorites $favorites)
+    {
+        //
+    }
+}
