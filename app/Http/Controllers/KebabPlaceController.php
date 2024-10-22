af05a41a4e1a36cb99aacc78f7f6880bfa72b592
zmigrOO <100092805+zmigrOO@users.noreply.github.com>
2024-10-22 21:10:47 +0200

    Create API endpoints Closes #2

---
+++ app/Http/Controllers/KebabPlaceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KebabPlace;
+use Illuminate\Http\Request;
+
+class KebabPlaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KebabPlace $kebabPlace)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KebabPlace $kebabPlace)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KebabPlace $kebabPlace)
+    {
+        //
+    }
+}
---
+++ app/Models/KebabPlace.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KebabPlace extends Model
+{
+    use HasFactory;
+}
