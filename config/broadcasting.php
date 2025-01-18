15e9d5d09df4face7234703a6882e16a4eb8c78f
Lee0z <102246039+Lee0z@users.noreply.github.com>
2025-01-18 15:00:52 +0100

    Update broadcasting configuration to use Pusher as default driver

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -3,28 +3,18 @@
 declare(strict_types=1);
 
 return [
-    "default" => env("BROADCAST_DRIVER", "null"),
+    "default" => env("BROADCAST_DRIVER", "pusher"),
 
     "connections" => [
         "pusher" => [
             'driver' => 'pusher',
-
             'key' => env('PUSHER_APP_KEY'),
-
             'secret' => env('PUSHER_APP_SECRET'),
-
             'app_id' => env('PUSHER_APP_ID'),
-
             'options' => [
-
-                'cluster' => env('PUSHER_CLUSTER'),
-
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-
-                'encrypted' => true,
-
                 'log' => true,
-
             ],
             "client_options" => [],
         ],
